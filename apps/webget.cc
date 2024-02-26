59bc5e808ae10da418098cd53830b13e756eddfe
Yizhen Zhang <98562104+LuminaScript@users.noreply.github.com>
2024-02-25 19:21:19 -0500

    Add files via upload

---
+++ apps/webget.cc
@@ -0,0 +1,67 @@
+#include "socket.hh"
+#include "address.hh"
+#include <cstdlib>
+#include <iostream>
+#include <span>
+#include <string>
+
+using namespace std;
+
+void get_URL( const string& host, const string& path )
+{
+
+    TCPSocket socket;
+
+    // Resolve the host name to an IP address
+  Address address(host, "http"); 
+
+    // Connect to the host
+    socket.connect(address);
+
+    // Format the HTTP GET request
+    string request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
+
+    // Send the GET request
+    socket.write(request);
+
+    // Read and display the response
+    while (!socket.eof()) {
+        string response;
+        socket.read(response);
+        cout << response;
+    }
+
+    // Close the socket
+    socket.close();
+}
+int main( int argc, char* argv[] )
+{
+  try {
+    if ( argc <= 0 ) {
+      abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
+    }
+
+    auto args = span( argv, argc );
+
+    // The program takes two command-line arguments: the hostname and "path" part of the URL.
+    // Print the usage message unless there are these two arguments (plus the program name
+    // itself, so arg count = 3 in total).
+    if ( argc != 3 ) {
+      cerr << "Usage: " << args.front() << " HOST PATH\n";
+      cerr << "\tExample: " << args.front() << " stanford.edu /class/cs144\n";
+      return EXIT_FAILURE;
+    }
+
+    // Get the command-line arguments.
+    const string host { args[1] };
+    const string path { args[2] };
+
+    // Call the student-written function.
+    get_URL( host, path );
+  } catch ( const exception& e ) {
+    cerr << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
